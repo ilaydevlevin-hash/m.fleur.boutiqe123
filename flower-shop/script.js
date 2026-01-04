f1db740a0861daa1a1ac87531d6026db0cb367ac
Ilay Levin <ilaylevin0911@gmail.com>
2026-01-04 19:41:47 +0200

    f3g

--- flower-shop/script.js
+++
@@ -1,8 +0,0 @@
-const sections = document.querySelectorAll('section');
-window.addEventListener('scroll', () => {
-  sections.forEach(sec => {
-    if (window.scrollY + window.innerHeight > sec.offsetTop + 100) {
-      sec.classList.add('visible');
-    }
-  });
-});
